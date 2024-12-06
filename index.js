919da5bee825f1d2b4b57f5aa5ae8d264ce03aa2
forysiak-vova <85363393+forysiak-vova@users.noreply.github.com>
2024-12-06 09:40:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
-const app = express();
-
-const PORT = process.env.PORT || 4000;
-
-app.get("/scrape", (req, res) => {
+import { createServer } from 'node:http';
+import scrapeLogic  from "./appScrapeLogic.js";
+const port = 80;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   scrapeLogic(res);
 });
-
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
 });
