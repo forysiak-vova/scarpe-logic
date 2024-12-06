7e9f33f7b402475c6fd6fe5edaed866e2ff77fe8
forysiak-vova <85363393+forysiak-vova@users.noreply.github.com>
2024-12-06 11:49:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import { createServer } from 'node:http';
 import scrapeLogic  from "./appScrapeLogic.js";
+import url from 'url';
 const port = 3000;
 const server = createServer((req, res) => {
+  let nr = url.parse(req.url, true).query['renderId'];
+  nr = Number(nr);
+  console.log(nr);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  scrapeLogic(res);
+  if(nr>0){
+    scrapeLogic(res, 1);
+  }else{
+    res.end(`Wrong id`);
+  }
 });
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
